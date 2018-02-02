59e20daba1318d38438d530f91621b9993bf48a9
anxic <antoine@sisaweb.com>
2018-02-02 20:35:10 +0100

    ADD SMTP CONFIG FILE

---
+++ application/config/smtp.php
@@ -0,0 +1,11 @@
+<?php
+$config['smtp'] = array(
+    "host" => '',
+    "username" => '',
+    "password" => '',
+    "port" => 587,
+    "tls" => true,
+    "emailFrom" => "",
+    "usernameFrom" => ""
+);
+?>
